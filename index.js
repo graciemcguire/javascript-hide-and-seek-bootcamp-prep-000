7860573d55e3d2ddcfec326f714f70015a8ceac7
Gracie McGuire <m.graciemcguire@gmail.com>
2018-09-25 20:05:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  let rankedList = document.querySelectorAll('ul.ranked-list li');
+  
+  for(let i = 0; i < rankedList.length; i++){
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+    
+  }
+}
+
+function deepestChild(){
+  let kids = document.querySelectorAll('#grand-node div');
+  
+  for (let i = 0; i < kids.length; i++){
+    if (!kids.length){
+    return false;
+  }
+  return kids[kids.length-1];
+ }
+}
